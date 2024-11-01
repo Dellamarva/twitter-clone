8445512d16407505ec90c4ef5f99657867a7df93
Greg Dellamura <g.dellamura@Gregs-MacBook-Pro-2.local>
2024-11-01 11:14:13 -0400

    Fixed async function for updateProfile

--- frontend/src/pages/profile/ProfilePage.jsx
+++ frontend/src/pages/profile/ProfilePage.jsx
@@ -151,7 +151,7 @@ const ProfilePage = () => {
 									<button
 										className='btn btn-primary rounded-full btn-sm text-white px-4 ml-2'
 										onClick={async () => {
-											updateProfile({coverImg, profileImg});
+											await updateProfile({coverImg, profileImg});
 											setProfileImg(null);
 											setCoverImg(null);
 										}}
